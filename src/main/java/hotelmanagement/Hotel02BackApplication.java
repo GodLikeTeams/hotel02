a4091e7c399c7aade4eaf749d1839c09614a781d
Agulator <1213931354@qq.com>
2020-05-16 21:15:38 +0800

    神呆呆的第一次测试

---
+++ src/main/java/hotelmanagement/Hotel02BackApplication.java
@@ -0,0 +1,13 @@
+package hotelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hotel02BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hotel02BackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hotelmanagement/Hotel02BackApplicationTests.java
@@ -0,0 +1,13 @@
+package hotelmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hotel02BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
